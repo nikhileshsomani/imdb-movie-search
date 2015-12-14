75ba8473244ede50eee21dad8ffe638ab1fc7737
nikhileshsomani <nikhilesh.somani@gmail.com>
2015-12-14 23:37:42 +0530

    empty input check

--- scripts/renderData.js
+++ scripts/renderData.js
@@ -12,7 +12,6 @@
          */
 
         handleScript: function(scriptElement, context, putInElement) {
-            // $('#overlay').remove();
             var templateScript = $(scriptElement).html(),
                 template = Handlebars.compile(templateScript),
                 compiledHTML = template(context);
